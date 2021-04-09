db1001f6e8469a012fd3f2ec7ca2b6a2e2d18f32
syfengcuhk <fengsym.ee@gmail.com>
2021-04-09 21:14:17 +0200

    update use of torchaudio.info

--- cpc/dataset.py
+++ cpc/dataset.py
@@ -410,8 +410,9 @@ class SameSpeakerSampler(Sampler):
 
 def extractLength(couple):
     speaker, locPath = couple
-    info = torchaudio.info(str(locPath))[0]
-    return info.length
+#(siyuan)    info = torchaudio.info(str(locPath))[0]
+    info = torchaudio.info(str(locPath))
+    return info.num_frames
 
 
 def findAllSeqs(dirName,
